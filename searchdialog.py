ecd811fd92a8de9ce6e6810edf338267ff5a056a
ageno-geo <93374955+ageno-geo@users.noreply.github.com>
2023-08-28 06:01:25 +0900

    Add files via upload
    グループタブの機能を追加
    例
          "group": "検索",

--- searchdialog.py
+++ searchdialog.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+from collections import OrderedDict
 
-from qgis.PyQt.QtWidgets import QDialog
+from qgis.PyQt.QtWidgets import QDialog, QTabWidget
 
 from qgis.PyQt import uic
 
@@ -13,6 +14,7 @@ from .widget.searchwidget import (
 
 
 UI_FILE = "dialog.ui"
+OTHER_GROUP_NAME = "その他"
 
 
 class SearchDialog(QDialog):
@@ -24,10 +26,17 @@ class SearchDialog(QDialog):
         self.init_gui(setting)
 
     def init_gui(self, setting):
+        self.tab_groups = self.create_tab_groups(setting["SearchTabs"])
         # create Page
         for tab_setting in setting["SearchTabs"]:
             page = self.create_page(tab_setting)
-            self.tabWidget.addTab(page, tab_setting["Title"])
+            if self.tab_groups:
+                tab_group_widget = self.tab_groups[
+                    tab_setting.get("group", OTHER_GROUP_NAME)
+                ]
+                tab_group_widget.addTab(page, tab_setting["Title"])
+            else:
+                self.tabWidget.addTab(page, tab_setting["Title"])
 
         self.set_window_title(0)
         self.tabWidget.currentChanged.connect(self.set_window_title)
@@ -39,9 +48,28 @@ class SearchDialog(QDialog):
     def create_page(self, setting):
         if setting["Title"] == "地番検索":
             return SearchTibanWidget(setting)
-        elif setting["Title"] == "所有者検索":
+        if setting["Title"] == "所有者検索":
             return SearchOwnerWidget(setting)
         return SearchTextWidget(setting)
 
+    def create_tab_groups(self, search_tabs):
+        tab_groups = OrderedDict()
+        for search_tab in search_tabs:
+            group_name = search_tab.get("group", OTHER_GROUP_NAME)
+            if group_name not in tab_groups:
+                group_widget = QTabWidget()
+                tab_groups[group_name] = group_widget
+        if len(tab_groups) <= 1:
+            return []
+        for group_name, group_widget in tab_groups.items():
+            self.tabWidget.addTab(group_widget, group_name)
+        return tab_groups
+
     def get_widgets(self):
+        if self.tab_groups:
+            return [
+                group_widget.widget(i)
+                for group_widget in self.tab_groups.values()
+                for i in range(group_widget.count())
+            ]
         return [self.tabWidget.widget(i) for i in range(self.tabWidget.count())]
